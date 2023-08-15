ea8d4e7c5cf54d0105d18227aa849700ac76d020
THARUSHKA11 <142327218+THARUSHKA11@users.noreply.github.com>
2023-08-15 20:24:28 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497078', 'me', true],
-  ['6281255369012', 'Ash', true], 
+  ['94741366868', 'me', true],
+  ['94741366868', 'THARUSHKA MAX', true], 
   [''] 
 ] //Number of owners
 
